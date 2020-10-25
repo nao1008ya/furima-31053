b652aa4bb12a06527050aced819affe129e436fe
nao1008ya <sinnao1008@gmail.com>
2020-10-25 19:11:06 +0900

    修正分１回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #  パスワードに半角英数字を両方含める
-  validates :password, format: { with: /\A[a-z\d]{6,100}+\z/i }
-
-  # カラムが空では保存できないというバリデーションを設定
-  validates :nickname, presence: true
-
-  # カラムが空且つ全角でないと保存できないというバリデーションを設定
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-
-  # カラムが空且つ全角カタカナでないと保存できないというバリデーションを設定
-  validates :first_kana_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
-  validates :last_kana_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
-
-  # カラムが数字で生年月日のバリデーションを設定
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    #  パスワードに半角英数字を両方含める
+    validates :password, format: { with: /\A[a-z\d]{6,100}+\z/i }
+    # カラムが空且つ全角でないと保存できないというバリデーションを設定
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :first_name
+      validates :last_name
+    end
+    # カラムが空且つ全角カタカナでないと保存できないというバリデーションを設定
+    with_options format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ } do
+      validates :first_kana_name
+      validates :last_kana_name
+    end
+  end
 
   # itemsテーブルとbuyersテーブルとcommentsテーブルのアソシエーションを設定
   has_many :items
