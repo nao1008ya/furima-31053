ec219d58a4dc865c66c1e2fb3ffa5ddb7947f023
nao1008ya <sinnao1008@gmail.com>
2020-10-29 11:21:25 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,6 +49,12 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
 
   private
 
