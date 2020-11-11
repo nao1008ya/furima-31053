f97804cec5a35374b08b6d77dbb516d10f87372d
nao1008ya <sinnao1008@gmail.com>
2020-11-11 16:55:46 +0900

    マイグレーションを編集, アソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   # アソシエーション設定
   belongs_to :user
   has_one :buyer
+  has_many :item_tag_relations
+  has_many :tags, through: :item_tag_relations
   # has_many :comments
 
   # ActiveStorage 各レコードとファイルを1対1の関係で紐付けるメソッド
