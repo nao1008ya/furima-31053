179aab17f3d5a41e99fd864ab27ed6b8d5d14853
nao1008ya <sinnao1008@gmail.com>
2020-11-14 19:45:19 +0900

    ウィザード形式のユーザー管理機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :nickname
-    validates :birthday
+    validates :nickname, :email, on: :create
+    validates :birthday, on: :create_name
     #  パスワードに半角英数字を両方含める
-    validates :password, format: { with: /\A[a-z\d]{6,100}+\z/i }
+    validates :password, format: { with: /\A[a-z\d]{6,100}+\z/i }, on: :create
     # カラムが空且つ全角でないと保存できないというバリデーションを設定
     with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
-      validates :first_name
-      validates :last_name
+      validates :first_name, on: :create_name
+      validates :last_name, on: :create_name
     end
     # カラムが空且つ全角カタカナでないと保存できないというバリデーションを設定
     with_options format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ } do
-      validates :first_kana_name
-      validates :last_kana_name
+      validates :first_kana_name, on: :create_name
+      validates :last_kana_name, on: :create_name
     end
   end
 
